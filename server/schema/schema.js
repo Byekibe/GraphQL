54fa51c6154b841e47c74356673461c6b4ce26d6
Byekibe <kibetpeter95@gmail.com>
2023-04-27 06:10:53 +0300

    Add type and queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,11 @@
 import { clients, projects } from '../sampleData.js'
-import { GraphQLID, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+import { 
+    GraphQLID, 
+    GraphQLList, 
+    GraphQLObjectType,
+    GraphQLSchema, 
+    GraphQLString 
+} from 'graphql'
 
 const ClientType = new GraphQLObjectType({
     name: 'Client',
@@ -11,6 +17,16 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+    })
+})
+
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
@@ -19,6 +35,28 @@ const RootQuery = new GraphQLObjectType({
             args: { id: { type: GraphQLID }},
             resolve(parent, args) {
                 return clients.find(client => client.id === args.id);
+            }
+        },
+
+        clients: {
+            type: new GraphQLList(ClientType),
+            resolve(parent, args) {
+                return clients
+            }
+        },
+
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return projects.find(project => project.id === args.id)
+            }
+        },
+
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects
             }
         }
     }
