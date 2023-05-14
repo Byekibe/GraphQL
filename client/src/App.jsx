230935ca2825268ec6bea1e898f41445bc21fe38
Byekibe <kibetpeter95@gmail.com>
2023-05-15 02:17:55 +0300

    Add create client and delete client UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { ApolloClient } from '@apollo/client';
 import Header from './components/Header.jsx';
-import Clients from './components/Clients.jsx'
+import Clients from './components/Clients.jsx';
+import AddClientModal from './components/AddClientModal.jsx';
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+          <AddClientModal />
           <Clients />
       </div>
     </>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,29 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App.jsx';
 import './index.css';
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+
+        projects: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        },
+      }
+    }
+  }
+});
+
 const client = new ApolloClient({
   uri: 'http://localhost:7000/graphql',
-  cache: new InMemoryCache(),
+  cache
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
